a53051ca29bfc815b26e3ee123d536f1b3871986
anantprakash01 <158019817+anantprakash01@users.noreply.github.com>
2024-06-14 19:52:46 +0530

    Admin Panel Update

--- E-Commerce/frontend/src/State/store.js
+++ E-Commerce/frontend/src/State/store.js
@@ -4,10 +4,12 @@ import authReducer from '../Redux/Auth/Reducer';
 import { customerProductReducer } from "./Product/Reducer";
 import { cartReducer } from "./Cart/Reducer";
 import { orderReducer } from "./Order/Reducer";
+import adminOrderReducer from "./Admin/Order/Reducer";
 const rootReducers=combineReducers({
     auth:authReducer,
     products:customerProductReducer,
     cart:cartReducer,
-    order:orderReducer
+    order:orderReducer,
+    adminOrder:adminOrderReducer
 });
 export  const store = legacy_createStore(rootReducers,applyMiddleware(thunk))
